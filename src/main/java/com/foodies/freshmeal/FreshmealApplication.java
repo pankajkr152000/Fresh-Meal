79fcdb46f80ee8936f7c6d605d6ba784e22d39a3
Pankaj Kumar <pankajkr152000@gmail.com>
2026-05-09 00:02:08 +0530

    foodies api new springboot application

---
+++ src/main/java/com/foodies/freshmeal/FreshmealApplication.java
@@ -0,0 +1,13 @@
+package com.foodies.freshmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foodies/freshmeal/FreshmealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodies.freshmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
